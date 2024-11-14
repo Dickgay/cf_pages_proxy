ec271c6fda3af759ca57cf6b0e62511d7876050f
Dickgay <47469884+Dickgay@users.noreply.github.com>
2024-11-14 21:39:59 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,13 +1,16 @@
 export default {
   async fetch(request, env) {
     let url = new URL(request.url);
-    let targetUrl = new URL(env.TARGET_URL); // 从环境变量中获取完整的目标 URL
-    
-    // 自动设置协议、主机名和端口
+    let targetUrl = new URL(env.TARGET_URL);
+
+    // 拼接路径和查询参数
+    url.pathname = url.pathname;
+    url.search = url.search;
     url.protocol = targetUrl.protocol;
     url.hostname = targetUrl.hostname;
     url.port = targetUrl.port;
 
+    // 保留请求方法、头部和主体，直接转发
     return fetch(new Request(url, request));
   },
 };
